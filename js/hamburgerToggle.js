8636939a1d27db7bef944b5bb4ddccfa8a99a39f
Arundhati Balasubramaniam <arundhatibalasubramaniam@Arundhatis-MacBook-Air.local>
2024-07-04 01:46:49 +0530

    change file paths

--- js/hamburgerToggle.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const hamburgerMenu = document.getElementById('hamburgerMenu');
-//     const mainMenuList = document.getElementById('mainMenuList');
-
-//     hamburgerMenu.addEventListener('click', function () {
-//         mainMenuList.classList.toggle('show');
-//     });
-// });
